349693beedeee572879945e13d948402c0340b0b
yumcyawiz <hashdolf1923@gmail.com>
2020-11-22 23:51:38 +0900

    add print

--- include/vec3.hpp
+++ include/vec3.hpp
@@ -1,6 +1,7 @@
 #ifndef _VEC3_H
 #define _VEC3_H
 #include <array>
+#include <iostream>
 
 class Vec3 {
  public:
@@ -9,6 +10,15 @@ class Vec3 {
   constexpr Vec3() : v({0, 0, 0}) {}
   constexpr Vec3(float _x) : v({_x, _x, _x}) {}
   constexpr Vec3(float _x, float _y, float _z) : v({_x, _y, _z}) {}
+
+  constexpr float x() const { return v[0]; }
+  constexpr float y() const { return v[1]; }
+  constexpr float z() const { return v[2]; }
 };
+
+std::ostream& operator<<(std::ostream& stream, const Vec3& v) {
+  stream << "(" << v.x() << ", " << v.y() << ", " << v.z() << ")";
+  return stream;
+}
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
+#include <iostream>
+
 #include "vec3.hpp"
 
 int main() {
   Vec3 v(0, 0, 0);
+  std::cout << v << std::endl;
   return 0;
 }
