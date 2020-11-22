2801f8d2f4df0ee0d5c00978baeb8b9df0f7f486
yumcyawiz <hashdolf1923@gmail.com>
2020-11-23 00:14:03 +0900

    add time count

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
+#include <chrono>
 #include <iostream>
 
 #include "vec3.hpp"
 
 int main() {
-  constexpr Vec3 v3 = Vec3(0, 1, 2) + Vec3(1, 2, 3);
+  Vec3 ret;
+  const auto start_time = std::chrono::system_clock::now();
+  for (unsigned int i = 0; i < 1e9; ++i) {
+    ret = ret + Vec3(0, 1, 2) + Vec3(3, 4, 5);
+  }
+  const auto end_time = std::chrono::system_clock::now();
+  std::cout << ret << std::endl;
+  std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end_time -
+                                                                     start_time)
+                   .count()
+            << std::endl;
+
   return 0;
 }
