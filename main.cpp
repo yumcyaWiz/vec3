ae2df3a391894fe9dc11739405c67e427364f1da
yumcyawiz <hashdolf1923@gmail.com>
2020-11-23 01:14:47 +0900

    vec3 vs vec3(expression-template)

--- main.cpp
+++ main.cpp
@@ -1,20 +1,47 @@
 #include <chrono>
 #include <iostream>
 
+#include "vec3-expression.hpp"
 #include "vec3.hpp"
 
 int main() {
   Vec3 ret;
-  const auto start_time = std::chrono::system_clock::now();
-  for (unsigned int i = 0; i < 1e9; ++i) {
-    ret = ret + Vec3(0, 1, 2) + Vec3(3, 4, 5);
+  Vec3 v1(0, 1, 2);
+  Vec3 v2(3, 4, 5);
+  Vec3 v3(3, 4, 5);
+  Vec3 v4(3, 4, 5);
+  auto start_time = std::chrono::system_clock::now();
+  for (unsigned int i = 0; i < 1e7; ++i) {
+    ret = v1;
+    ret = v1 + v2;
+    ret = v1 + v2 + v3;
+    ret = v1 + v2 + v3 + v4;
   }
-  const auto end_time = std::chrono::system_clock::now();
+  auto end_time = std::chrono::system_clock::now();
   std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end_time -
                                                                      start_time)
                    .count()
             << std::endl;
   std::cout << ret << std::endl;
+
+  Vec3E ret2;
+  Vec3E v1e(0, 1, 2);
+  Vec3E v2e(3, 4, 5);
+  Vec3E v3e(3, 4, 5);
+  Vec3E v4e(3, 4, 5);
+  start_time = std::chrono::system_clock::now();
+  for (unsigned int i = 0; i < 1e7; ++i) {
+    ret2 = v1e;
+    ret2 = v1e + v2e;
+    ret2 = v1e + v2e + v3e;
+    ret2 = v1e + v2e + v3e + v4e;
+  }
+  end_time = std::chrono::system_clock::now();
+  std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end_time -
+                                                                     start_time)
+                   .count()
+            << std::endl;
+  std::cout << ret2 << std::endl;
 
   return 0;
 }
