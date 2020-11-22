ca48e48e8e822468f793f7fcf5f0757c0a6403d3
yumcyawiz <hashdolf1923@gmail.com>
2020-11-23 01:14:25 +0900

    impl expression template vec3

---
+++ include/vec3-expression.hpp
@@ -0,0 +1,56 @@
+#ifndef _VEC3_EXPRESSION_H
+#define _VEC3_EXPRESSION_H
+#include <array>
+#include <iostream>
+
+class Vec3E {
+ public:
+  std::array<float, 3> v;
+
+  constexpr Vec3E() : v({0, 0, 0}) {}
+  constexpr Vec3E(float _x) : v({_x, _x, _x}) {}
+  constexpr Vec3E(float _x, float _y, float _z) : v({_x, _y, _z}) {}
+
+  constexpr float x() const { return v[0]; }
+  constexpr float y() const { return v[1]; }
+  constexpr float z() const { return v[2]; }
+  constexpr float operator[](std::size_t i) const { return v[i]; }
+
+  template <typename E>
+  constexpr Vec3E& operator=(const E& e) {
+    for (unsigned int i = 0; i < 3; ++i) {
+      v[i] = e[i];
+    }
+    return *this;
+  }
+};
+
+inline std::ostream& operator<<(std::ostream& stream, const Vec3E& v) {
+  stream << "(" << v.x() << ", " << v.y() << ", " << v.z() << ")";
+  return stream;
+}
+
+template <typename L, typename Op, typename R>
+class Expression {
+ private:
+  const L& l;
+  const R& r;
+
+ public:
+  Expression(const L& _l, const R& _r) : l(_l), r(_r) {}
+
+  constexpr float operator[](std::size_t i) const {
+    return Op::apply(l[i], r[i]);
+  }
+};
+
+struct Plus {
+  static constexpr float apply(float lhs, float rhs) { return lhs + rhs; }
+};
+
+template <typename L, typename R>
+inline constexpr Expression<L, Plus, R> operator+(const L& lhs, const R& rhs) {
+  return Expression<L, Plus, R>(lhs, rhs);
+}
+
+#endif
