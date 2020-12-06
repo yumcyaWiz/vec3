b2d941bcedf7351f8a0720646b78b9f68ec438da
yumcyawiz <hashdolf1923@gmail.com>
2020-12-06 19:23:24 +0900

    add dot, cross on Vec3E

--- include/vec3-expression.hpp
+++ include/vec3-expression.hpp
@@ -60,4 +60,13 @@ inline constexpr Expression<L, Minus, R> operator-(const L& lhs, const R& rhs) {
   return Expression<L, Minus, R>(lhs, rhs);
 }
 
+inline constexpr float dot(const Vec3E& v1, const Vec3E& v2) {
+  return v1.x() * v2.x() + v1.y() * v2.y() + v1.z() * v2.z();
+}
+inline constexpr Vec3E cross(const Vec3E& v1, const Vec3E& v2) {
+  return Vec3E(v1.y() * v2.z() - v1.z() * v2.y(),
+               v1.z() * v2.x() - v1.x() * v2.z(),
+               v1.x() * v2.y() - v1.y() * v2.x());
+}
+
 #endif
