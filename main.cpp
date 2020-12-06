b003616a3b15ca8e11b8ef2ad713098d80755c3a
yumcyawiz <hashdolf1923@gmail.com>
2020-12-06 19:21:54 +0900

    add test code with generic lambda

--- main.cpp
+++ main.cpp
@@ -1,25 +1,43 @@
 #include <chrono>
+#include <cmath>
 #include <iostream>
 
 #include "vec3-expression.hpp"
 #include "vec3.hpp"
 
 int main() {
+  const unsigned int samples = 1000000;
+
+  auto test_func = [](const auto& center, float radius, const auto& origin,
+                      const auto& direction) {
+    const float b = dot(direction, origin - center);
+    const float c = dot(origin - center, origin - center) - radius * radius;
+    const float D = b * b - c;
+
+    const float t1 = -b - std::sqrt(D);
+    const float t2 = -b + std::sqrt(D);
+    float t = t1;
+    if (t < 0) {
+      t = t2;
+      if (t < 0) {
+        return false;
+      }
+    }
+    return true;
+  };
+
   Vec3 ret;
-  Vec3 v1(0, 1, 2);
-  Vec3 v2(3, 4, 5);
-  Vec3 v3(3, 4, 5);
-  Vec3 v4(3, 4, 5);
   auto start_time = std::chrono::system_clock::now();
-  for (unsigned int i = 0; i < 1e7; ++i) {
-    ret = v1 + v2 + v3 + v4;
+  int hit_count = 0;
+  for (unsigned int i = 0; i < samples; ++i) {
+    hit_count += test_func(Vec3(0, 0, 0), 1.0, Vec3(0, 0, -3), Vec3(0, 0, 1));
   }
   auto end_time = std::chrono::system_clock::now();
   std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end_time -
                                                                      start_time)
                    .count()
             << std::endl;
-  std::cout << ret << std::endl;
+  std::cout << hit_count << std::endl;
 
   Vec3E ret2;
   Vec3E v1e(0, 1, 2);
@@ -27,7 +45,7 @@ int main() {
   Vec3E v3e(3, 4, 5);
   Vec3E v4e(3, 4, 5);
   start_time = std::chrono::system_clock::now();
-  for (unsigned int i = 0; i < 1e7; ++i) {
+  for (unsigned int i = 0; i < samples; ++i) {
     ret2 = v1e + v2e + v3e + v4e;
   }
   end_time = std::chrono::system_clock::now();
