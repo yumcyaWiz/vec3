d46aa3b60527c812e4b7eda0867e683314e1f042
yumcyawiz <hashdolf1923@gmail.com>
2020-12-07 01:40:58 +0900

    random vec3 direction

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <chrono>
 #include <cmath>
 #include <iostream>
+#include <random>
 
 #include "vec3-expression.hpp"
 #include "vec3.hpp"
 
 int main() {
-  const unsigned int samples = 1000000;
+  const unsigned int samples = 10000000;
 
   auto test_func = [](const auto& center, float radius, const auto& origin,
                       const auto& direction) {
@@ -26,25 +27,34 @@ int main() {
     return true;
   };
 
+  std::random_device seed_gen;
+  std::mt19937 rng(seed_gen());
+
+  unsigned int hit_count = 0;
   auto start_time = std::chrono::system_clock::now();
   for (unsigned int i = 0; i < samples; ++i) {
-    test_func(Vec3(0, 0, 0), 1.0, Vec3(0, 0, -3), Vec3(0, 0, 1));
+    hit_count += test_func(Vec3(0, 0, 0), 1.0, Vec3(0, 0, -3),
+                           Vec3(rng(), rng(), rng()));
   }
   auto end_time = std::chrono::system_clock::now();
   std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end_time -
                                                                      start_time)
                    .count()
             << std::endl;
+  std::cout << hit_count << std::endl;
 
+  hit_count = 0;
   start_time = std::chrono::system_clock::now();
   for (unsigned int i = 0; i < samples; ++i) {
-    test_func(Vec3E(0, 0, 0), 1.0, Vec3E(0, 0, -3), Vec3E(0, 0, 1));
+    hit_count += test_func(Vec3E(0, 0, 0), 1.0, Vec3E(0, 0, -3),
+                           Vec3E(rng(), rng(), rng()));
   }
   end_time = std::chrono::system_clock::now();
   std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end_time -
                                                                      start_time)
                    .count()
             << std::endl;
+  std::cout << hit_count << std::endl;
 
   return 0;
 }
