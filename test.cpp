a0b3e66237210ee9e1f6203724b07d22226fbfca
yumcyawiz <hashdolf1923@gmail.com>
2020-12-07 09:13:32 +0900

    test

--- test.cpp
+++ test.cpp
@@ -7,11 +7,14 @@
 int main() {
   const Vec3 v = Vec3(0, 1, 2) + Vec3(3, 4, 5) + Vec3(6, 7, 8);
 
-  const auto ve1 = Vec3E(0, 1, 2) + Vec3E(3, 4, 5);
-  const Vec3E ve2(0, 0, 0);
-  float ve3 = (ve1 + ve2)[0];
+  const auto v1 = Vec3E(0, 1, 2);
+  const auto v2 = Vec3E(3, 4, 5);
+  const auto v3 = Vec3E(6, 7, 8);
+  const Vec3E ve1 = v1 + v2 + v3;
 
   std::cout << v << std::endl;
-  std::cout << ve3 << std::endl;
+  std::cout << ve1 << std::endl;
+  const Vec3E v4 = Vec3E(0, 1, 2) + Vec3E(3, 4, 5) + Vec3E(6, 7, 8);
+  std::cout << v4 << std::endl;
   return 0;
 }
